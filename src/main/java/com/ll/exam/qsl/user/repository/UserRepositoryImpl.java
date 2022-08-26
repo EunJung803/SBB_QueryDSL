5523f30f94b0b7c179530c604ef725180ecd4d54
EunJung803 <aimee3322@gmail.com>
2022-08-26 14:33:57 +0900

    Test/Fix : getQslUser 수정 -> 모든 id에 대해서 찾을 수 있도록 재구현

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -32,7 +32,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
         return jpaQueryFactory
                 .select(QSiteUser.siteUser)
                 .from(QSiteUser.siteUser)
-                .where(QSiteUser.siteUser.id.eq(1L))
+                .where(QSiteUser.siteUser.id.eq(id))
                 .fetchOne();
     }
 }
