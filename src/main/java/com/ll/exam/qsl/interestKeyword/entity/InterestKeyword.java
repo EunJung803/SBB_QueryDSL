2cdab20fb78f7a134aecd939291710ef502c4fb9
EunJung803 <aimee3322@gmail.com>
2022-09-02 13:27:12 +0900

    Feat : @EqualsAndHashCode.Include 도입 hashCode와 equals 메서드 자동생성처리됨

--- src/main/java/com/ll/exam/qsl/interestKeyword/entity/InterestKeyword.java
+++ src/main/java/com/ll/exam/qsl/interestKeyword/entity/InterestKeyword.java
@@ -12,21 +12,9 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class InterestKeyword {
     @Id
+    @EqualsAndHashCode.Include
     private String content;
-
-    // Generate로 추가
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        InterestKeyword that = (InterestKeyword) o;
-        return content.equals(that.content);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(content);
-    }
 }
